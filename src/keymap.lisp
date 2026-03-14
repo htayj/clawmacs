6dc265eb7efa1770c0729bf7e3b3053fe4887e6b
Tay <hardy.taylor.j@gmail.com>
2026-03-14 19:08:08 -0400

    feat(keymap): Add keymap with parent-chain lookup and default bindings
    Implement keymaps as hash tables with parent chain for fallback
    lookup. keymap-bind maps keys to command symbols, keymap-lookup
    walks the chain. The default keymap binds standard editing keys
    (C-a, C-e, C-k, C-y, backspace) and chat keys (RET to send,
    C-j for newline).

--- src/keymap.lisp
+++ src/keymap.lisp
@@ -1,1 +1,59 @@
 (in-package :clawmacs)
+
+;;; --------------------------------------------------------------------------
+;;; Keymap
+;;; --------------------------------------------------------------------------
+
+(defclass keymap ()
+  ((name     :initarg :name
+             :reader keymap-name
+             :type keyword)
+   (bindings :initarg :bindings
+             :reader keymap-bindings
+             :type hash-table)
+   (parent   :initarg :parent
+             :reader keymap-parent
+             :initform nil
+             :type (or null keymap)))
+  (:documentation "A key-to-command mapping with optional parent chain for fallback."))
+
+(declaim (ftype (function (keyword &key (:parent (or null keymap))) keymap) make-keymap))
+(defun make-keymap (name &key parent)
+  "Create a new empty keymap with NAME and optional PARENT for fallback."
+  (make-instance 'keymap
+    :name name
+    :bindings (make-hash-table :test #'equal)
+    :parent parent))
+
+(declaim (ftype (function (keymap t symbol) keymap) keymap-bind))
+(defun keymap-bind (keymap key command)
+  "Bind KEY to COMMAND in KEYMAP."
+  (setf (gethash key (keymap-bindings keymap)) command)
+  keymap)
+
+(declaim (ftype (function (keymap t) (or null symbol)) keymap-lookup))
+(defun keymap-lookup (keymap key)
+  "Look up KEY in KEYMAP, walking the parent chain if not found."
+  (or (gethash key (keymap-bindings keymap))
+      (when (keymap-parent keymap)
+        (keymap-lookup (keymap-parent keymap) key))))
+
+;;; --------------------------------------------------------------------------
+;;; Default Keymap
+;;; --------------------------------------------------------------------------
+
+(defvar *default-keymap* nil
+  "The default keymap for chat buffers. Initialized by init-default-keymap.")
+
+(defun init-default-keymap ()
+  "Build and install the default keymap with standard chat buffer bindings."
+  (let ((km (make-keymap :default)))
+    (keymap-bind km #\Return 'send-message)
+    (keymap-bind km #\Linefeed 'insert-newline-command)
+    (keymap-bind km #\Soh 'beginning-of-line-command)     ; C-a = ASCII 1
+    (keymap-bind km #\Enq 'end-of-line-command)           ; C-e = ASCII 5
+    (keymap-bind km #\Vt 'kill-line-command)              ; C-k = ASCII 11
+    (keymap-bind km #\Em 'yank-command)                   ; C-y = ASCII 25
+    (keymap-bind km #\Backspace 'delete-char-backward-command)
+    (keymap-bind km #\Rubout 'delete-char-backward-command)
+    (setf *default-keymap* km)))
--- tests/keymap-test.lisp
+++ tests/keymap-test.lisp
@@ -1,1 +1,33 @@
 (in-package :clawmacs/tests)
+
+(def-suite keymap-suite
+  :description "Keymap tests"
+  :in clawmacs-suite)
+
+(in-suite keymap-suite)
+
+(test keymap-bind-and-lookup
+  "Binding a key and looking it up returns the command."
+  (let ((km (make-keymap :test)))
+    (keymap-bind km #\a 'some-command)
+    (is (eq 'some-command (keymap-lookup km #\a)))
+    (is (null (keymap-lookup km #\b)))))
+
+(test keymap-parent-chain-lookup
+  "Lookup falls back to parent keymap."
+  (let* ((parent (make-keymap :parent))
+         (child (make-keymap :child :parent parent)))
+    (keymap-bind parent #\a 'parent-command)
+    (keymap-bind child #\b 'child-command)
+    (is (eq 'child-command (keymap-lookup child #\b)))
+    (is (eq 'parent-command (keymap-lookup child #\a)))
+    (is (null (keymap-lookup parent #\b)))))
+
+(test keymap-child-overrides-parent
+  "Child bindings shadow parent bindings for the same key."
+  (let* ((parent (make-keymap :parent))
+         (child (make-keymap :child :parent parent)))
+    (keymap-bind parent #\a 'parent-version)
+    (keymap-bind child #\a 'child-version)
+    (is (eq 'child-version (keymap-lookup child #\a)))
+    (is (eq 'parent-version (keymap-lookup parent #\a)))))
