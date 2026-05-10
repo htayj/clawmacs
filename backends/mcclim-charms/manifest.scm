93432d4dbb120201c37427eef9beef501971e1bf
Tay <hardy.taylor.j@gmail.com>
2026-05-10 03:58:24 -0400

    chore(backends): Remove mcclim-charms backend

--- backends/mcclim-charms/manifest.scm
+++
@@ -1,8 +0,0 @@
-(specifications->manifest
- '("sbcl"
-   "sbcl-alexandria"
-   "sbcl-fiveam"
-   "sbcl-mcclim"
-   "sbcl-cl-charms"
-   "ncurses"
-   "python"))
--- backends/mcclim-charms/tests/package.lisp
+++
@@ -1,8 +0,0 @@
-(defpackage #:mcclim-charms/tests
-  (:use #:cl #:fiveam)
-  (:local-nicknames (#:backend #:mcclim-charms)))
-
-(in-package #:mcclim-charms/tests)
-
-(def-suite mcclim-charms-suite
-  :description "Unit tests for the McCLIM charms backend.")
